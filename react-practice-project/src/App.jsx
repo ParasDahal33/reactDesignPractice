defb356da26899f89476c9c461bd3a44a030b323
ParasDahal33 <paras.dahal2016@gmail.com>
2023-01-02 16:59:45 +0545

    Finished setting structure and layout

--- react-practice-project/src/App.jsx
+++ react-practice-project/src/App.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import styles from './style';
+import {
+  Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+} from './components'
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+const App = () => (
+  <div className='bg-primary w-full overflow-hidden'>
+    <div className={`${styles.paddingx} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+
+      <div className={`${styles.boxWidth}`}>
+        <Stats />
+        <Billing />
+        <Business />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footer />
+
+      </div>
+    </div>
+
+  </div>
+);
 
 export default App
